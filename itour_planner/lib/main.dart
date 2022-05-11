3e8929f95ca68c32c56bc13e48cb7474e45f1d20
Abdullah1071 <abdullahshahid1071@gmail.com>
2022-05-11 16:39:58 +0500

    added splash screen class into separate file and cleaned main.dart

--- itour_planner/lib/main.dart
+++ itour_planner/lib/main.dart
@@ -1,9 +1,5 @@
-import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:itour_planner/reset_password.dart';
-import 'package:itour_planner/verified_screen.dart';
-import 'package:page_transition/page_transition.dart';
-import './login_screen.dart';
+import './splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,54 +14,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: SplashScreen(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text('iTour Planner'),
-          backgroundColor: const Color.fromRGBO(173, 37, 51, 1),
-        ),
-        body: const Center(
-            child: Text(
-          'iTour Planner',
-          style: TextStyle(fontSize: 40),
-        )));
-  }
-}
-
-class SplashScreen extends StatelessWidget {
-  const SplashScreen({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedSplashScreen(
-      splash: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          SizedBox(
-            height: 78,
-            child: Image.asset("assets/images/app_logo.png"),
-          ),
-        ],
-      ),
-      nextScreen: const LoginScreen(),
-      backgroundColor: const Color.fromRGBO(173, 37, 51, 1),
-      splashTransition: SplashTransition.decoratedBoxTransition,
-      duration: 1000,
-      pageTransitionType: PageTransitionType.bottomToTop,
-      animationDuration: const Duration(seconds: 1),
     );
   }
 }
---
+++ itour_planner/lib/splash_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:page_transition/page_transition.dart';
+import './login_screen.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSplashScreen(
+      splash: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: 78,
+            child: Image.asset("assets/images/app_logo.png"),
+          ),
+        ],
+      ),
+      nextScreen: const LoginScreen(),
+      backgroundColor: const Color.fromRGBO(173, 37, 51, 1),
+      splashTransition: SplashTransition.decoratedBoxTransition,
+      duration: 1000,
+      pageTransitionType: PageTransitionType.bottomToTop,
+      animationDuration: const Duration(seconds: 1),
+    );
+  }
+}
