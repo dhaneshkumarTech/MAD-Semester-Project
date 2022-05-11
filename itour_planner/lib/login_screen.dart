5e00cea83499e9a09bc6513231b928535aa5a1ad
Abdullah1071 <abdullahshahid1071@gmail.com>
2022-05-11 13:43:57 +0500

    made the design better

--- itour_planner/lib/login_screen.dart
+++ itour_planner/lib/login_screen.dart
@@ -23,17 +23,13 @@ class _LoginScreenState extends State<LoginScreen> {
         children: [
           Container(
             width: double.infinity,
-            height: 300,
+            height: 250,
             child: SizedBox(
               height: 60,
               child: Image.asset('assets/images/app_logo.png'),
             ),
             alignment: Alignment.center,
             decoration: const BoxDecoration(
-              borderRadius: BorderRadius.only(
-                bottomRight: Radius.circular(200.0),
-                bottomLeft: Radius.circular(160.0),
-              ),
               color: Color.fromRGBO(173, 37, 51, 1),
               gradient: LinearGradient(
                 begin: Alignment.topRight,
@@ -50,92 +46,94 @@ class _LoginScreenState extends State<LoginScreen> {
             height: 420,
             margin: const EdgeInsets.fromLTRB(20, 0, 20, 0),
             decoration: const BoxDecoration(
-              borderRadius: BorderRadius.all(Radius.circular(30.0)),
+              borderRadius: BorderRadius.all(
+                Radius.circular(30.0),
+              ),
             ),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
               children: <Widget>[
-                const SizedBox(
-                  height: 100,
-                ),
-                TextFormField(
-                  validator: null,
-                  controller: null,
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    prefixIcon: const Icon(
-                      Icons.mail_outline,
-                      color: Color.fromRGBO(173, 37, 51, 1),
-                    ),
-                    errorBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                      borderSide: const BorderSide(
-                        width: 2.0,
+                Container(
+                  margin: const EdgeInsets.fromLTRB(5, 50, 5, 0),
+                  child: TextFormField(
+                    validator: null,
+                    controller: null,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      prefixIcon: const Icon(
+                        Icons.mail_outline,
+                        color: Color.fromRGBO(173, 37, 51, 1),
                       ),
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                      borderSide: const BorderSide(
-                        width: 2.0,
+                      errorBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                        borderSide: const BorderSide(
+                          width: 2.0,
+                        ),
                       ),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                      borderSide: const BorderSide(
-                        width: 2.0,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                        borderSide: const BorderSide(
+                          width: 2.0,
+                        ),
                       ),
-                    ),
-                    labelText: 'Email or phone number',
-                    labelStyle: const TextStyle(
-                      color: Color.fromRGBO(173, 37, 51, 1),
-                    ),
-                  ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                TextFormField(
-                  validator: null,
-                  obscureText: _isHidden,
-                  controller: null,
-                  decoration: InputDecoration(
-                    prefixIcon: const Icon(
-                      Icons.lock,
-                      color: Color.fromRGBO(173, 37, 51, 1),
-                    ),
-                    suffix: InkWell(
-                      onTap: _togglePasswordView,
-                      child: Icon(
-                        _isHidden ? Icons.visibility_off : Icons.visibility,
-                        color: const Color.fromRGBO(173, 37, 51, 1),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                        borderSide: const BorderSide(
+                          width: 2.0,
+                        ),
                       ),
-                    ),
-                    errorBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                      borderSide: const BorderSide(
-                        width: 2.0,
+                      labelText: 'Email or phone number',
+                      labelStyle: const TextStyle(
+                        color: Color.fromRGBO(173, 37, 51, 1),
                       ),
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                      borderSide: const BorderSide(
-                        width: 2.0,
-                      ),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: const BorderSide(
-                        width: 2.0,
-                      ),
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                    labelText: 'Password',
-                    labelStyle: const TextStyle(
-                      color: Color.fromRGBO(173, 37, 51, 1),
                     ),
                   ),
                 ),
                 Container(
-                  margin: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+                  margin: const EdgeInsets.fromLTRB(5, 20, 5, 0),
+                  child: TextFormField(
+                    validator: null,
+                    obscureText: _isHidden,
+                    controller: null,
+                    decoration: InputDecoration(
+                      prefixIcon: const Icon(
+                        Icons.lock,
+                        color: Color.fromRGBO(173, 37, 51, 1),
+                      ),
+                      suffix: InkWell(
+                        onTap: _togglePasswordView,
+                        child: Icon(
+                          _isHidden ? Icons.visibility_off : Icons.visibility,
+                          color: const Color.fromRGBO(173, 37, 51, 1),
+                        ),
+                      ),
+                      errorBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                        borderSide: const BorderSide(
+                          width: 2.0,
+                        ),
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                        borderSide: const BorderSide(
+                          width: 2.0,
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                          width: 2.0,
+                        ),
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      labelText: 'Password',
+                      labelStyle: const TextStyle(
+                        color: Color.fromRGBO(173, 37, 51, 1),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.fromLTRB(0, 15, 0, 0),
                   alignment: Alignment.centerRight,
                   child: TextButton(
                     child: const Text(
@@ -151,8 +149,8 @@ class _LoginScreenState extends State<LoginScreen> {
                 ),
                 Container(
                   margin: const EdgeInsets.fromLTRB(0, 30, 0, 0),
-                  width: 250,
-                  height: 40,
+                  width: 150,
+                  height: 50,
                   child: ElevatedButton(
                     style: ElevatedButton.styleFrom(
                       alignment: Alignment.center,
@@ -161,14 +159,35 @@ class _LoginScreenState extends State<LoginScreen> {
                       shape: const StadiumBorder(),
                     ),
                     onPressed: () {},
-                    child: const Text('Login'),
+                    child: const Text(
+                      'LOG IN',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
                   ),
                 ),
+                Container(
+                  margin: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      const Text("Don't have an account?"),
+                      TextButton(
+                        onPressed: () {},
+                        child: const Text(
+                          'Sign Up',
+                        ),
+                        style: TextButton.styleFrom(
+                          primary: const Color.fromRGBO(173, 37, 51, 1),
+                        ),
+                      ),
+                    ],
+                  ),
+                )
               ],
             ),
           ),
           Container(
-            margin: const EdgeInsets.all(20),
+            margin: const EdgeInsets.fromLTRB(20, 20, 20, 0),
             alignment: Alignment.bottomRight,
             width: double.infinity,
             child: SizedBox(
