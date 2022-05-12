756685113231419eb43d8e50eeff5faef4c3e51d
Abdullah1071 <abdullahshahid1071@gmail.com>
2022-05-12 18:27:42 +0500

    made changes to notifications panel and made a new screen named review reviewed screen

--- itour_planner/lib/splash_screen.dart
+++ itour_planner/lib/splash_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:animated_splash_screen/animated_splash_screen.dart';
-import 'package:itour_planner/notification_screen.dart';
+import 'package:itour_planner/ReviewsScreen/reviews_reviewed_screen.dart';
 import 'package:page_transition/page_transition.dart';
 
 class SplashScreen extends StatelessWidget {
@@ -18,7 +18,7 @@ class SplashScreen extends StatelessWidget {
           ),
         ],
       ),
-      nextScreen: const NotificationScreen(),
+      nextScreen: const ReviewsReviewedScreen(),
       backgroundColor: const Color.fromRGBO(173, 37, 51, 1),
       splashTransition: SplashTransition.scaleTransition,
       duration: 1000,
