15e84f072a61e26ccf70f2b800cbf484dc32383d
Dhanesh Kumar <dkumar@gmail.com>
2022-05-14 21:00:50 +0500

    Merge branch 'main' of https://github.com/Abdullah1071/itour-planner

--- itour_planner/lib/SplashScreen/splash_screen.dart
+++ itour_planner/lib/SplashScreen/splash_screen.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:animated_splash_screen/animated_splash_screen.dart';
-<<<<<<< HEAD
 import 'package:itour_planner/main.dart';
-=======
-import 'package:itour_planner/DashBoardScreen/dashboard_screen.dart';
->>>>>>> 3751fb020ed78ab15a72ff84d129e62761b577aa
 import 'package:page_transition/page_transition.dart';
 
 class SplashScreen extends StatelessWidget {
@@ -14,7 +10,6 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AnimatedSplashScreen(
-<<<<<<< HEAD
         splash: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
@@ -31,24 +26,6 @@ class SplashScreen extends StatelessWidget {
         pageTransitionType: PageTransitionType.bottomToTop,
         animationDuration: const Duration(seconds: 1),
       );
-=======
-      splash: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          SizedBox(
-            height: 78,
-            child: Image.asset("assets/images/app_logo.png"),
-          ),
-        ],
-      ),
-      nextScreen: const DashboardScreen(),
-      backgroundColor: const Color.fromRGBO(173, 37, 51, 1),
-      splashTransition: SplashTransition.scaleTransition,
-      duration: 1000,
-      pageTransitionType: PageTransitionType.bottomToTop,
-      animationDuration: const Duration(seconds: 1),
-    );
->>>>>>> 3751fb020ed78ab15a72ff84d129e62761b577aa
   }
 }
 
